a36224dbf2e4746f481767c35f41034394d091fb
Emanuel <emanuel.costa.dev@gmail.com>
2025-09-11 20:58:35 -0300

    create member class

--- backend/church/src/main/java/br/com/emanuelcosta/church/entities/Member.java
+++ backend/church/src/main/java/br/com/emanuelcosta/church/entities/Member.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
 @Table(name = "tb_member")
@@ -25,4 +26,150 @@ public class Member implements Serializable {
     private String address;
     private String state;
     private String occupation;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "church_id")
+    private Church church;
+
+    public Member() {
+    }
+
+    public Member(Long id, String status, String role, LocalDate baptismDate, String admission, String name, String gender, LocalDate birthdate, String address, String state, String occupation, Church church) {
+        this.id = id;
+        this.status = status;
+        this.role = role;
+        this.baptismDate = baptismDate;
+        this.admission = admission;
+        this.name = name;
+        this.gender = gender;
+        this.birthdate = birthdate;
+        this.address = address;
+        this.state = state;
+        this.occupation = occupation;
+        this.church = church;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public LocalDate getBaptismDate() {
+        return baptismDate;
+    }
+
+    public void setBaptismDate(LocalDate baptismDate) {
+        this.baptismDate = baptismDate;
+    }
+
+    public String getAdmission() {
+        return admission;
+    }
+
+    public void setAdmission(String admission) {
+        this.admission = admission;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public Church getChurch() {
+        return church;
+    }
+
+    public void setChurch(Church church) {
+        this.church = church;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return Objects.equals(id, member.id) && Objects.equals(status, member.status) && Objects.equals(role, member.role) && Objects.equals(baptismDate, member.baptismDate) && Objects.equals(admission, member.admission) && Objects.equals(name, member.name) && Objects.equals(gender, member.gender) && Objects.equals(birthdate, member.birthdate) && Objects.equals(address, member.address) && Objects.equals(state, member.state) && Objects.equals(occupation, member.occupation);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, status, role, baptismDate, admission, name, gender, birthdate, address, state, occupation);
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", status='" + status + '\'' +
+                ", role='" + role + '\'' +
+                ", baptismDate=" + baptismDate +
+                ", admission='" + admission + '\'' +
+                ", name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", birthdate=" + birthdate +
+                ", address='" + address + '\'' +
+                ", state='" + state + '\'' +
+                ", occupation='" + occupation + '\'' +
+                ", churchId=" + (church != null ? church.getId() : null) +
+                '}';
+    }
 }
---
+++ backend/church/src/main/java/br/com/emanuelcosta/church/entities/Study.java
@@ -0,0 +1,4 @@
+package br.com.emanuelcosta.church.entities;
+
+public class Study {
+}
