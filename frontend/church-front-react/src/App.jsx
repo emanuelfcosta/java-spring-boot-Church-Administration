4d097d5afe628320ec2aef05f75c23ec2a4a0c29
Emanuel <emanuel.costa.dev@gmail.com>
2025-09-20 02:30:33 -0300

    creating Header Component

--- frontend/church-front-react/src/App.jsx
+++ frontend/church-front-react/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import ListChurchComponent from "./components/Churches/ListChurchComponent";
+import HeaderComponent from "./components/HeaderComponent";
 
 function App() {
   return (
     <>
+      <HeaderComponent />
       <ListChurchComponent />
     </>
   );
