e111bb0d336ad4d6970d442b87e5d0b079c9aede
Emanuel <emanuel.costa.dev@gmail.com>
2025-09-20 02:59:03 -0300

    using routes

--- frontend/church-front-react/src/App.jsx
+++ frontend/church-front-react/src/App.jsx
@@ -1,12 +1,19 @@
 import "./App.css";
 import ListChurchComponent from "./components/Churches/ListChurchComponent";
 import HeaderComponent from "./components/HeaderComponent";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <HeaderComponent />
-      <ListChurchComponent />
+      <BrowserRouter>
+        <HeaderComponent />
+
+        <Routes>
+          {/*http://localhost:5173/churches */}
+          <Route path="/churches" element={<ListChurchComponent />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
