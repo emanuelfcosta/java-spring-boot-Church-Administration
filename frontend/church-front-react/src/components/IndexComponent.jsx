4c96990b022dfb3e0eb6f7c6938e5b260f02a424
Emanuel <emanuel.costa.dev@gmail.com>
2025-09-27 19:01:19 -0300

    update main page

--- frontend/church-front-react/src/components/IndexComponent.jsx
+++ frontend/church-front-react/src/components/IndexComponent.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const IndexComponent = () => {
-  return <div>Main Page</div>;
+  return (
+    <>
+      <div className="alert alert-warning text-center" role="alert">
+        <h2>Church Management System</h2>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <img
+              className="mx-auto d-block"
+              src="/main-image.jpg"
+              alt="Vet Clinic"
+              style={{ maxWidth: "95%" }}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default IndexComponent;
